4f4034f96dd971b420cb7a5a382afdf5abf5db6b
singularitti <singularitti@outlook.com>
2020-04-29 23:00:54 -0400

    Implement type `Touch`

--- src/FileRules.jl
+++ src/FileRules.jl
@@ -1,5 +1,21 @@
 module FileRules
 
-# Write your package code here.
+using FilePathsBase: Path, hasparent
+
+export Touch
+
+struct Touch
+    path
+    filemode::Integer
+    dirmode::Unsigned
+end
+Touch(path) = Touch(path, 0o777, 0o777)
+function (t::Touch)()
+    p = expanduser(t.path) |> Path
+    if hasparent(p)
+        mkpath(joinpath(splitpath(p)[1:end-1]...); mode = t.dirmode)
+    end
+    return chmod(touch(p), t.filemode)
+end
 
 end

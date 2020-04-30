5197efe4e93b9bd9003135f5a06dded8a60397c6
singularitti <singularitti@outlook.com>
2020-04-29 23:33:27 -0400

    Implement type `Write` & update `Touch`

--- src/FileRules.jl
+++ src/FileRules.jl
@@ -1,11 +1,11 @@
 module FileRules
 
-using FilePathsBase: Path, hasparent
+using FilePathsBase: Path, hasparent, iswritable
 
-export Touch
+export Touch, Write
 
-struct Touch
-    path
+struct Touch{T}
+    path::T
     filemode::Integer
     dirmode::Unsigned
 end
@@ -16,6 +16,32 @@ function (t::Touch)()
         mkpath(joinpath(splitpath(p)[1:end-1]...); mode = t.dirmode)
     end
     return chmod(touch(p), t.filemode)
+end
+
+struct Write{T}
+    path::T
+    mode::AbstractString
+    function Write{T}(path, mode) where {T}
+        @assert string(lowercase(mode)) ∈ ("r", "w", "a", "r+", "w+", "a+")
+        return new(path, lowercase(mode))
+    end
+end
+Write(path::T, mode) where {T} = Write{T}(path, mode)
+function (w::Write)(x)
+    p = expanduser(w.path) |> Path
+    if !isfile(p)
+        Touch(p)()
+    end
+    if !iswritable(p)
+        error("file $p is not writable!")
+    end
+    if w.mode ∈ ("w", "w+")
+        @info "try to overwrite file $p, a backup file will be created!"
+        Touch(p * ".bkp")()
+    end
+    open(p, w.mode) do io
+        write(io, x)
+    end
 end
 
 end
